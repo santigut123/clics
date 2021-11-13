be3af165208bc83820befc509c205bd058bee6f2
Santiago  Gutierrez <santigut@posteo.net>
2021-11-13 15:25:50 -0500

    Added some net stuff

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
-	//"net"
-	//"os"
+	"net"
+	"io"
+	"time"
 )
-func main() {
-	fmt.Println("Cool we're printing some shit...\nVery nice.")	
+type ChatServer struct{
 
+}
+
+func main() {
+	fmt.Println("Cool we're printing some shit...\nVery nice.")
+	ln,err := net.Listen("tcp",":9000")
+	conn,err := ln.Accept()
+	defer ln.Close()
+	if err != nil{
+		panic(err)
+	}
+	io.WriteString(conn,fmt.Sprint("Hello World \n",time.Now(),"\n"))
 }
