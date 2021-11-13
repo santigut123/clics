de6780d6f6100678ef5a35872fe2c71953549f61
rbugge <ryan.bugge@tuta.io>
2021-11-13 18:49:08 -0500

    server/client example

--- client/client.go
+++ client/client.go
@@ -1,13 +1,31 @@
-package client
+// package client
 
-type Client struct {
-	port int
-}
+// type Client struct {
+// 	port int
+// }
 
-func MakeClient() *Client {
-	newClient := Client{port: 1}
-	return &newClient
-}
-func (c *Client) SetPort(port int) {
-	c.port = port
+// func MakeClient() *Client {
+// 	newClient := Client{port: 1}
+// 	return &newClient
+// }
+// func (c *Client) SetPort(port int) {
+// 	c.port = port
+// }
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		// handle error
+	}
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println(status)
 }
--- main.go
+++ main.go
@@ -2,26 +2,38 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"time"
 )
 
 type ChatServer struct {
 }
 
 func main() {
-	fmt.Println("Cool we're printing some shit...\nVery nice.")
-	ln, err := net.Listen("tcp", ":9000")
-	conn, err := ln.Accept()
+	// fmt.Println("Cool we're printing some shit...\nVery nice.")
+	// ln, err := net.Listen("tcp", ":9000")
+	// conn, err := ln.Accept()
+	// defer ln.Close()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// io.WriteString(conn, fmt.Sprint("Hello World \n", time.Now(), "\n"))
+
+	// buf := make([]byte, 1024)
+	// conn.Read(buf)
+	// conn.Write([]byte("Writing to connection..."))
+
+	ln, err := net.Listen("tcp", ":8080")
 	defer ln.Close()
 	if err != nil {
-		panic(err)
+		// handle error
 	}
-	io.WriteString(conn, fmt.Sprint("Hello World \n", time.Now(), "\n"))
-
-	buf := make([]byte, 1024)
-	conn.Read(buf)
-	conn.Write([]byte("Writing to connection..."))
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+			continue
+		}
+		fmt.Println(conn)
+	}
 
 }
